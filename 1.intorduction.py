2e9f7c4f99582be8049dd6496dca5af5845bbe39
heysushil <sushil.chaudhary111@gmail.com>
2020-06-08 19:01:20 +0530

    introduction of python

---
+++ 1.intorduction.py
@@ -0,0 +1,118 @@
+'''
+Topics:
+
+introduction
+syntax
+comment
+variable
+'''
+
+
+print('Hello how are you')
+print('Hello how are you')
+
+# condion
+if 4 > 1:
+    a = 7
+    b = 10
+    sumval = a + b
+    print('\nSUmval: ',sumval)
+    print('\nEnd of outer if')
+
+    # inner if condtion
+    if 2 > 1:
+        print('\nYEs this inner condtion is rihgt')
+
+        # loo in inner condtion
+        for i in range(6):
+            print('Loop: ',i)
+
+
+'''
+PHP Code for if condtion:
+
+if(condiont){
+    body of if
+}
+
+c / c++ / php / java / aps.net
+'''
+
+
+# condtion sysntex
+multilineCommenVal = '''
+if condtion:
+    body of if
+
+
+if and else syntax:
+    
+    if condtion:
+        body of if
+    else:
+        body else
+
+if elif else:
+    
+    if condtion:
+        body of if
+    elif condtion:
+        body of elif
+    else:
+        body of else
+
+foor loop :
+
+    for intitialized valuese in values:
+        body of for
+
+
+All styntax
+
+condtion
+loop
+    while
+    foot
+function
+class
+'''
+print(multilineCommenVal)
+
+# single line comment -> ctr + /
+
+'''
+multiline comment
+'''
+
+print('Hello how are you where are you\n\n              This is second line\n\n\n   Hello 3rd line')
+
+
+'''
+Varaible:
+
+1. variable jo single value ko store karta hain
+2. vrabiel multiple values store karta hai useing:
+    list
+    tuple
+    set
+    dictonary
+'''
+
+a = 6
+ab = (1,2,3,4,5,6,7,8,9)
+print('ab type: ',type(ab))
+
+print('Use Number: ',ab[0])
+print('Use Number: ',ab[1])
+print('Use Number: ',ab[2])
+print('Use Number: ',ab[3])
+
+
+'''
+Question:
+
+1. What is intentationin python?
+2. array me push anad pop kya hota hai?
+3. array me search and sort kaise use karte hain?
+'''
+
