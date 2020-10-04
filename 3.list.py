b4e1a8a264060b294074c76b023abe6ddbe46ab1
heysushil <sushil.chaudhary111@gmail.com>
2020-10-04 18:43:51 +0530

    change

--- 3.list.py
+++ 3.list.py
@@ -5,71 +5,36 @@
 # और हाँ GitHub पर मुझे फॉलो करना ना भूलो ताकि सारे अपडेट एण्ड वर्क आपको मिलता रहे। 
 
 '''
-List: []
+Python Collections:
 
-list values ko index ke form rakhta hai
-list me values ko: new value add/ dellet / remove/ change
-list me hum single value, list , tuple, set, dict
+Ye 4 datatypes multiple values ko hold ya store karne ki capacity rakte hain. Is liye hi inhe collection bhi kha jata hai.
+Example: Abhi tak humne int,float,comple aur set datatype use kiya but ye sabhi ek time pe ek value ko hi store kar sakte hain.
+Isliye jab hame ek sath multiple values ko store karna ho single varibale me to hum yehai 4 datatypes ka use karte hain.
+
+1. list => denote by []
+2. tupe => ()
+3. dict (dictonary) => {}
+4. set => {}
+
+Python List me following methods hain:
+
+Note: Example ke liye mylist = [1,2,3,4], iske behalf par examples diye gaye hain:
+
+1. append(): List me last se 1 nai value add karne ke liye append ka use kiya jata hai. Example: mylist.append(5)
+2. clear(): clear method list ko empty karta hai. Empty karne par agar varibel ko print karoge to empty list milega. But error nahi milega. Example: mylist.clear()
+3. copy(): Copy method ek list ko copy karke new variable me store kar dega. Example: newlist = mylist.copy()
+4. extend(): Extend ka use karke ek se jada list ko aapsh me marge kiya jata hai. Exaple: mylist.extend(newlist)
+5. index(): Index method kisi bhi value ka index position bata hai. Example: mylist.index(4)
+6. insert(): Insert method list me existing index possition par new value ko update karta hai. Example: mylist.index(1, 11), yaha par(indexPossion, newValue)
+7. pop(): Pop method thik append ka ulta hai. Ye by default list me last se ek value ko remove karta hai. Otherwise kisi specific index value ko remove karne ke liye is method me index postion dena hota hai. Example: mylist.pop() - Ye last se ek value remove kardega. mylist.pop(2) - Ye list ke 2nd index ki value ko remove kardega.
+8. remove(): Remove ka use kiya jata hai jab aapko index postion ki jagh par value ka pata ho. To direct value ko remove karne ke liye remove method ka use karte hain. Example: mylist.remove(4) - Ye list me jaha bhi 4 hai useko remove karega.
+9. reverse(): Reverse list ko ulta kardega. Means list by default assending order me hota hai. To ye list ko desending order me convert kar dega. Example: mylist.reverse()
+10. sort(): Sort mehtod list ko assending order me sort kardega. Example: mylist.sort() yaha par sort(revers=Ture/False) Ture = Ye list ko desending order me sort karega. False = Ye assending order me sort karega.
+11. count(): Count method list me present kisi bhi dublicate values ko count karne ke liye use hota hai. Example: mylist.count(4)
 '''
 
-# list
-newlist = [11,12,13,14,15,16]
-
-# check data type by type()
-print('\n\nWelcome to list')
-print('Datatype of newlist: ',type(newlist))
-
-# fetch index value: list index value always start from 0
-print(newlist)
-print('newlist[0]: ',newlist[3])
-# newlist[startpoint:endpoint]
-print('define start point: ',newlist[3:])
-print('define end point: ',newlist[:3]) # n-1
-print('start and end point: ',newlist[1:3])
-
-# same start and end poitn using negative value
-
-print('negative number newlist[-1]: ',newlist[-1])
-print('negative stant and end point: ',newlist[-4:-1])
-
-# update new value on list index possition
-newlist[0] = 111
-newlist.insert(1,122)
-
-# find lenght of list
-print('Check list length: ',len(newlist))
-
-# add new number in list
-newlist.append(17)
-
-# remove list value
-newlist.remove(17)
-
-# reove list value by index posstion
-newlist.pop(5)
-
-# delete list or value
-del newlist[0]
-
-# clear
-# newlist.clear()
-
-# copy list
-nextlist = newlist.copy()
-
-# join
-thirdlist = newlist + nextlist
-newlist.extend(thirdlist)
-
-# list()
-oldlist = [newlist,nextlist,thirdlist]
 
 
-print('\nNewlist: ',newlist)
-print('Nextlist: ',nextlist)
-print('Thiredlist: ',thirdlist)
-print('\n\nOldlist: ',oldlist)
-print(oldlist[2][2])
 '''
 Questions: 
 
