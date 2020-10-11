7eeec44fafcbec7f666a2dc97554621cbfaccd31
heysushil <sushil.chaudhary111@gmail.com>
2020-10-11 21:27:35 +0530

    bool method

---
+++ 2.4.boolean.py
@@ -0,0 +1,33 @@
+'''
+Boolean Values:
+
+1. Boolean values alwarys return Ture or False
+2. Boolean values 0 ko False aur 1 to True as accept karta hai
+3. Boolean method is bool()
+
+Boolean Values kab False return karta hai:
+
+1. bool(False)
+2. bool(None)
+3. bool(0)
+4. bool('')
+5. bool([])
+6. bool(())
+7. bool({})
+
+Note: String me concatenae  or 1 se jada stirng ko marge karne ke liye hum + sign ka use karte hain.
+C, C++ languages me concatenation ke liye .(dot) sign ka use kiya jata hain.
+'''
+
+# bool with False
+print('\nUse false in bool: ', bool(False))
+print('\nNone: ', bool(None))
+print('\nEmpy string: ', bool(''))
+
+# bool with True
+print('\nIf 19 > 10: ', 19 > 10)
+print('\nCHeck rangm: ', 768687 > 8798798)
+
+# usgin bool method
+print('\nValue on bool(): ', bool('1ahdfklahlkdf'))
+print('\ncheck: ', 10 >= 10)
