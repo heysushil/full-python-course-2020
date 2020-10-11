5919663d8cf991979b88e6fbe330c2b3423e2061
heysushil <sushil.chaudhary111@gmail.com>
2020-10-11 16:00:00 +0530

    number system

---
+++ 2.2.number_system.py
@@ -0,0 +1,42 @@
+'''
+Number system:
+
+1. int: interger: -/+ 10 20 -40
+2. flaot: 10.10 -22.2000
+3. complex: 29j
+
+Real Number system:
+
+1. binary
+2. octal
+3. decimal
+4. hexa decimal
+'''
+
+# interger
+a = 10
+b = -200
+print('\nInter: ', a, ' Type: ', type(a))
+print('\nInter with negative: ', b, ' Type: ', type(b))
+
+# float
+f = 20.20
+print('\nFloat: ', f, ' Type: ', type(f))
+
+# complex
+c = 20j
+print('\nComplex: ', c, ' Type: ', type(c))
+
+'''
+Real Number system:
+
+1. binary 0b
+2. octal 0o (421)
+3. decimal 0d
+4. hexa decimal 0x
+'''
+
+print('\nBinary: ', 0b111)
+print('\nOctal: ', 0o1011011)
+# print('\nDecimal: ', 0d101001)
+print('\nHexa: ', 0xAF1)
